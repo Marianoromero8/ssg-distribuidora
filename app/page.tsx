df6166c41e3d54bd339248c438323ac5c78234f4
Marianoromero8 <marianoromero97.mr@gmail.com>
2025-01-15 12:19:35 -0300

    En proceso...

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,28 @@ import NavBar from "./components/navBar/NavBar";
 import Home from "./components/home/Home"
 import AboutUs from "./components/aboutUs/AboutUs";
 import Brands from "./components/brands/Brands";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Page() {
   return (
-    <div className="flex flex-col sm:px-10 w-full overflow-x-hidden">
-      <NavBar />
-      <Home />
-      <Brands />
-      <AboutUs />
+    // <div className="flex flex-col sm:px-10 w-full overflow-x-hidden">
+    <div className="flex flex-col items-center justify-center mt-10 ">
+      <h1 className="text-5xl text-[#f17900ec] font-bold">SSG DISTRIBUIDORA</h1>
+      <Link href="https://www.youtube.com/watch?v=WV-DdHG6T_s&ab_channel=SantiGraziano" target="_blank">
+        <Image
+          src='/construccion.jpg'
+          width={700}
+          height={700}
+          alt="En proceso...."
+          sizes="100vw"
+        />
+      </Link>
     </div>
+    //     <NavBar />
+    //     <Home />
+    //     <Brands />
+    //     <AboutUs /> 
+    //   </div>
   );
 }
