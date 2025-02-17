3efa2ec10c3b74697b384bf96d292f75681db537
Marianoromero8 <marianoromero97.mr@gmail.com>
2025-02-17 20:14:36 -0300

    add: some products

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import NavBar from "./components/navBar/NavBar";
-import { Ubuntu } from "next/font/google";
-
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -13,13 +11,6 @@ const geistSans = Geist({
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
-});
-
-const ubuntu = Ubuntu({
-  weight: ["400"],
-  style: ["italic"],
-  subsets: ["latin"],
-  variable: "--font-ubuntu",
 });
 
 export const metadata: Metadata = {
