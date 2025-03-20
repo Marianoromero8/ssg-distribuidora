40d4c3dab1749a750eee825642dbbe2e1b7e5fc7
Marianoromero8 <marianoromero97.mr@gmail.com>
2025-03-20 19:32:44 -0300

    fix: responsive design in process

--- app/components/navBar/NavBar.tsx
+++ app/components/navBar/NavBar.tsx
@@ -14,12 +14,16 @@ import Link from "next/link";
 import brands from "../../../public/data/products.json"
 import { usePathname, useRouter } from "next/navigation";
 import { motion } from 'framer-motion'
+import { useState } from "react";
+import { Menu, X } from "lucide-react"; // Íconos para el menú
 
 
 //Le saque el focus a los DropdownMenuSubTrigger y DropdownMenuItem
 export default function NavBar() {
     const router = useRouter();
     const pathname = usePathname();
+    const [menuOpen, setMenuOpen] = useState(false);
+
 
     const scrollToSection = (sectionId: string) => {
         setTimeout(() => {
@@ -39,6 +43,7 @@ export default function NavBar() {
         } else {
             router.push("/#aboutus"); // Redirige y luego hace scroll
         }
+        setMenuOpen(false);
     };
 
     return (
@@ -59,7 +64,12 @@ export default function NavBar() {
                 </Link>
             </motion.div>
 
-            <div className="flex flex-row justify-start items-center text-[#4166e0] ps-14 gap-40 w-screen">
+            <button className="md:hidden" onClick={() => setMenuOpen(!menuOpen)}>
+                {menuOpen ? <X size={28} className="text-[#4166e0]" /> : <Menu size={28} className="text-[#4166e0]" />}
+            </button>
+
+            {/*Menu escritorio */}
+            <div className="hidden sm:flex flex-row justify-start items-center text-[#4166e0] ps-14 gap-40 w-screen">
                 {/* Sobre nosotros */}
                 <DropdownMenu>
                     <DropdownMenuTrigger onClick={handleAboutUsClick} className="transition-transform duration-300 ease-in-out transform hover:scale-105">
@@ -260,6 +270,168 @@ export default function NavBar() {
                     </DropdownMenuTrigger>
                 </DropdownMenu>
             </div>
+
+
+            {/* Menú en móviles */}
+            {menuOpen && (
+                <div className="sm:hidden absolute top-14 left-0 w-full shadow-lg flex flex-col items-center space-y-4 p-4 text-[#4166e0] z-40">
+                    <DropdownMenu>
+                        <DropdownMenuTrigger onClick={handleAboutUsClick} className="transition-transform duration-300 ease-in-out transform hover:scale-105">
+                            Sobre nosotros
+                        </DropdownMenuTrigger>
+                    </DropdownMenu>
+                    <DropdownMenu>
+                        <DropdownMenuTrigger className="transition-transform duration-300 ease-in-out transform hover:scale-105">
+                            Marcas
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent className="text-[#ffff] bg-[#4166e0] max-h-48 overflow-y-auto">
+                            <DropdownMenuSub>
+                                {brands.map((options) => (
+                                    <Link href={`/brands/${options.brand}`} key={options.id}>
+                                        <DropdownMenuItem>
+                                            {options.brand}
+                                        </DropdownMenuItem>
+                                    </Link>
+                                ))}
+                            </DropdownMenuSub>
+                        </DropdownMenuContent>
+                    </DropdownMenu>
+                    <DropdownMenu>
+                        <Link href={"/products"}>
+                            <DropdownMenuTrigger className="transition-transform duration-300 ease-in-out transform hover:scale-105">
+                                Productos
+                            </DropdownMenuTrigger>
+                        </Link>
+                        <DropdownMenuContent className="text-[#ffff] bg-[#4166e0]">
+                            <Link href={"/products"}>
+                                <DropdownMenuItem>
+                                    Todos
+                                </DropdownMenuItem>
+                            </Link>
+                            <DropdownMenuSub>
+                                <DropdownMenuSubTrigger onClick={() => {
+                                    window.location.href = "/products/congelados"; // Redirige manualmente
+                                }}>
+                                    Congelados
+                                </DropdownMenuSubTrigger>
+                                <DropdownMenuSubContent className="text-[#ffff] bg-[#4166e0]">
+                                    <Link href="/products/congelados/papas%20fritas">
+                                        <DropdownMenuItem>
+                                            Papas Fritas
+                                        </DropdownMenuItem>
+                                    </Link>
+                                    <Link href="/products/congelados/hamburguesas">
+                                        <DropdownMenuItem>
+                                            Hamburguesas
+                                        </DropdownMenuItem>
+                                    </Link>
+                                    <Link href="/products/congelados/frutas">
+                                        <DropdownMenuItem>
+                                            Frutas
+                                        </DropdownMenuItem>
+                                    </Link>
+                                    <Link href="/products/congelados/verduras">
+                                        <DropdownMenuItem>
+                                            Verduras
+                                        </DropdownMenuItem>
+                                    </Link>
+                                </DropdownMenuSubContent>
+                            </DropdownMenuSub>
+                            <DropdownMenuSub>
+                                <DropdownMenuSubTrigger
+                                    onClick={() => {
+                                        window.location.href = "/products/refrigerados"; // Redirige manualmente
+                                    }}
+                                >
+                                    Refrigerados
+                                </DropdownMenuSubTrigger>
+                                <DropdownMenuSubContent className="text-[#ffff] bg-[#4166e0]">
+                                    <Link href="/products/refrigerados/aderezos">
+                                        <DropdownMenuItem>
+                                            Aderezos
+                                        </DropdownMenuItem>
+                                    </Link>
+                                    <Link href="/products/refrigerados/lacteos">
+                                        <DropdownMenuItem>
+                                            Lacteos
+                                        </DropdownMenuItem>
+                                    </Link>
+                                    <Link href="/products/refrigerados/quesos">
+                                        <DropdownMenuItem>
+                                            Quesos
+                                        </DropdownMenuItem>
+                                    </Link>
+                                    <Link href="/products/refrigerados/fiambres">
+                                        <DropdownMenuItem>
+                                            Fiambres
+                                        </DropdownMenuItem>
+                                    </Link>
+                                    <Link href="/products/refrigerados/salchichas">
+                                        <DropdownMenuItem>
+                                            Salchichas
+                                        </DropdownMenuItem>
+                                    </Link>
+                                </DropdownMenuSubContent>
+                            </DropdownMenuSub>
+                            <DropdownMenuSub>
+                                <DropdownMenuSubTrigger
+                                    onClick={() => {
+                                        window.location.href = "/products/secos"; // Redirige manualmente
+                                    }}
+                                >
+                                    Secos
+                                </DropdownMenuSubTrigger>
+                                <DropdownMenuSubContent className="text-[#ffff] bg-[#4166e0]">
+                                    <Link href="/products/secos/yerba%20mate">
+                                        <DropdownMenuItem>
+                                            Yerba Mate
+                                        </DropdownMenuItem>
+                                    </Link>
+                                    <Link href="/products/secos/panes">
+                                        <DropdownMenuItem>
+                                            Panes
+                                        </DropdownMenuItem>
+                                    </Link>
+                                    <Link href="/products/secos/food%20service">
+                                        <DropdownMenuItem>
+                                            Food Service
+                                        </DropdownMenuItem>
+                                    </Link>
+                                </DropdownMenuSubContent>
+                            </DropdownMenuSub>
+                        </DropdownMenuContent>
+                    </DropdownMenu>
+                    <DropdownMenu>
+                        <DropdownMenuTrigger className="transition-transform duration-300 ease-in-out transform hover:scale-105">
+                            Clientes
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent className="text-[#ffff] bg-[#4166e0]">
+                            <Link href="https://www.instagram.com/reina.burguesa/" target="_blank">
+                                <DropdownMenuItem className="cursor-pointer">
+                                    Reina
+                                </DropdownMenuItem>
+                            </Link>
+                            <DropdownMenuSeparator />
+                            <Link href="https://www.instagram.com/madd.burgers/" target="_blank">
+                                <DropdownMenuItem className="cursor-pointer">
+                                    Madd
+                                </DropdownMenuItem>
+                            </Link>
+                            <DropdownMenuSeparator />
+                            <Link href="https://www.instagram.com/salvaje.burger/" target="_blank">
+                                <DropdownMenuItem className="cursor-pointer">
+                                    Fogón Salvaje
+                                </DropdownMenuItem>
+                            </Link>
+                        </DropdownMenuContent>
+                    </DropdownMenu>
+                    <DropdownMenu>
+                        <DropdownMenuTrigger onClick={() => scrollToSection('contacts')} className="transition-transform duration-300 ease-in-out transform hover:scale-105">
+                            Contacto
+                        </DropdownMenuTrigger>
+                    </DropdownMenu>
+                </div>
+            )}
         </div>
     );
 }
